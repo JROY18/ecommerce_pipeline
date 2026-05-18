302504816600cc80df8a7e1791a872528337a8df
Joseph Roy <jinuroy2002@gmail.com>
2026-05-18 18:48:03 +0530

    New transformations added

--- transformation/transform_orders.py
+++ transformation/transform_orders.py
@@ -1,6 +1,6 @@
 import os
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import (col, date_diff, to_timestamp, round, avg, count, sum)
+from pyspark.sql.functions import (col, datediff, to_timestamp, round, avg, count, sum, date_format)
 
 spark = SparkSession.builder\
         .appName("EcEcommerce Pipeline Transformation")\
@@ -34,7 +34,7 @@ df = df.withColumn("order_purchase_timestamp", to_timestamp(col("order_purchase_
        .withColumn("order_delivered_customer_date", to_timestamp(col("order_delivered_customer_date")))\
        .withColumn("order_estimated_delivery_date", to_timestamp(col("order_estimated_delivery_date")))
 
-df = df.withColumn("delivery_time_days", date_diff(col("order_delivered_customer_date"), col("order_purchase_timestamp")))
+df = df.withColumn("delivery_time_days", datediff(col("order_delivered_customer_date"), col("order_purchase_timestamp")))
 
 df = df.withColumn("total_order_value", round(col("price") + col("freight_value") ,2))  
 
@@ -52,6 +52,21 @@ revenue_by_state = df.groupBy("customer_state")\
 
 avg_delivery = df.filter(col("delivery_time_days").isNotNull())\
                  .agg(round(avg("delivery_time_days"), 2).alias("avg_delivery_days"))
+
+# 3. Revenue by payment 
+
+revenue_by_payment = df.groupBy("payment_type") \
+    .agg(round(sum("payment_value"), 2).alias("total_revenue"),
+         count("order_id").alias("total_orders")) \
+    .orderBy("total_revenue", ascending=False)
+
+
+# 4. Monthly Revenue
+
+monthly_revenue = df.groupBy(date_format("order_purchase_timestamp", "yyyy-MM").alias("month")).agg(round(sum("payment_value"), 2).alias("total_revenue")) \
+    .orderBy("month")
+
+# Actions
 
 revenue_by_state.show()
 
